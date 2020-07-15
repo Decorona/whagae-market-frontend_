94a555d8b640cd572446836f06a180ba99948685
mrkyel <kyel100@naver.com>
2020-07-15 23:55:34 +0900

    [feat]마이페이지 퍼블리싱수정

--- src/pages/MyPage/MyPageComponent/index.js
+++ src/pages/MyPage/MyPageComponent/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { colors, fonts } from "../../../constants";
+import MyPageButton from "../../../components/MyPageButton";
 
 const styles = StyleSheet.create({
   MyPageContainer: {
@@ -20,13 +21,23 @@ const styles = StyleSheet.create({
     flex: 0.5,
     borderBottomColor: colors.grey,
   },
+  MyPageUserTitletext: {
+    fontSize: 18,
+    fontWeight: "500",
+    fontStyle: "normal",
+    letterSpacing: -1.05,
+    textAlign: "center",
+    fontFamily: fonts.Medium,
+    color: colors.grey,
+  },
   MyPageTitletext: {
-    fontSize: 14,
+    fontSize: 18,
     fontWeight: "500",
     fontStyle: "normal",
     lineHeight: 20,
     letterSpacing: -1.05,
     textAlign: "center",
+    paddingBottom: 15,
     fontFamily: fonts.Medium,
     color: colors.grey,
   },
@@ -52,41 +63,49 @@ const styles = StyleSheet.create({
     fontFamily: fonts.Medium,
     color: colors.grey,
   },
-  userIcon: {
+  UserProfileArea: {
     flex: 0.5,
+    marginTop: 30,
   },
   preVillage: {
     flex: 0.5,
+    paddingTop: 20,
+    width: 300,
+    borderWidth: 0.1,
+    marginBottom: 25,
+    backgroundColor: colors.greyf0,
+  },
+  UserIcon: {
+    width: 38,
+    height: 38,
+    borderRadius: 19,
+    backgroundColor: colors.greyb5,
+    marginLeft: 6,
+    marginTop: 5.5,
   },
 });
 
 const MyPage = () => {
-  const onPressButton = () => {
-    alert("버튼클릭");
-  };
   return (
     <View style={styles.MyPageContainer}>
       <View style={styles.MyPageContainerUp}>
-        <View style={styles.userIcon}>
-          <Text style={styles.MyPageTitletext}>USER</Text>
+        <View style={styles.UserProfileArea}>
+          <Text style={styles.UserIcon}></Text>
+          <Text style={styles.MyPageUserTitletext}>USER</Text>
         </View>
         <View style={styles.preVillage}>
           <Text style={styles.MyPageTitletext}>화개멤버 전용 혜택</Text>
           <Text style={styles.MyPageSubTitletext}>모든 상품 100%무료배송</Text>
           <Text style={styles.MyPageSubTitletext}>5% 포인트 적립</Text>
-          <Button
-            onPress={onPressButton}
-            title="지금 무료체험 신청하기"
-            color="lightgray"
-          />
+          <MyPageButton title={"지금 무료체험 신청하기"} />
         </View>
       </View>
       <View style={styles.MyPageContainerDown}>
-        <Button onPress={onPressButton} title="찜한가게" color="lightgray" />
-        <Button onPress={onPressButton} title="주문모곩" color="lightgray" />
-        <Button onPress={onPressButton} title="화개 멤버십" color="lightgray" />
-        <Button onPress={onPressButton} title="구매후기" color="lightgray" />
-        <Button onPress={onPressButton} title="고객센터" color="lightgray" />
+        <MyPageButton title={"찜한가게"} />
+        <MyPageButton title={"주문목록"} />
+        <MyPageButton title={"화개 멤버십"} />
+        <MyPageButton title={"구매후기"} />
+        <MyPageButton title={"고객센터"} />
       </View>
     </View>
   );
