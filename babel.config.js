2fbba81e8a4f5a941affbc77821868e14f4de960
Daniel <elrion018@gmail.com>
2020-07-23 00:30:11 +0900

    [chore]바벨 root 세팅 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,48 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "babel-plugin-root-import",
+      {
+        paths: [
+          {
+            rootPathSuffix: "./src/pages",
+            rootPathPrefix: "@pages",
+          },
+          {
+            rootPathSuffix: "./src/components",
+            rootPathPrefix: "@components",
+          },
+          {
+            rootPathSuffix: "./src/constants",
+            rootPathPrefix: "@constants",
+          },
+          {
+            rootPathSuffix: "./src/constants",
+            rootPathPrefix: "@constants",
+          },
+          {
+            rootPathSuffix: "./src/system",
+            rootPathPrefix: "@system",
+          },
+          {
+            rootPathSuffix: "./src/reducers",
+            rootPathPrefix: "@reducers",
+          },
+          {
+            rootPathSuffix: "./src/assets",
+            rootPathPrefix: "@assets",
+          },
+          {
+            rootPathSuffix: "./src/actions",
+            rootPathPrefix: "@actions",
+          },
+          {
+            rootPathSuffix: "./src/utils",
+            rootPathPrefix: "@utils",
+          },
+        ],
+      },
+    ],
+  ],
 };
